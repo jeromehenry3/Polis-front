b3c41dba75c61d7905552277efd8c67d2602bfdc
Fabien Mackowiak <fmackodev@gmail.com>
2019-04-30 13:42:35 +0200

    Animation inputs

--- src/components/Signin/index.js
+++ src/components/Signin/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/label-has-for */
 /**
  * Import
  */
@@ -6,7 +8,8 @@ import React from 'react';
  * Local import
  */
 // Composants
-import Logo from '../../images/logo-saumon.png';
+import Logo from '../../styles/images/logo-saumon.png';
+
 // Styles et assets
 import './signin.scss';
 
@@ -18,12 +21,15 @@ const Signin = () => {
   // Je récupère le focus d'un input
   const handleFocus = (e) => {
     // J'ajoute la classe focus à l'élément focus, ce qui passe sa bordure en orange.
+    console.log(e.target.previousSibling);
+    e.target.previousSibling.classList.add('focus');
     e.target.classList.add('focus');
   };
 
   // Je récupère le blur d'un input
   const handleBlur = (e) => {
     // Je remove la classe focus, ce qui redonnera à l'élément la bordure de base
+    e.target.previousSibling.classList.remove('focus');
     e.target.classList.remove('focus');
   };
 
@@ -38,15 +44,31 @@ const Signin = () => {
 
         <form action="">
           <div className="signin-container_names">
-            <input type="text" name="last-name" placeholder="Nom" onClick={handleFocus} onBlur={handleBlur} />
-            <input type="text" name="first-name" placeholder="Prénom" onClick={handleFocus} onBlur={handleBlur} />
+            <div>
+              <label htmlFor="last-name">Nom</label>
+              <input type="text" id="last-name" name="last-name" placeholder="Nom" onClick={handleFocus} onBlur={handleBlur} />
+            </div>
+
+            <div>
+              <label htmlFor="first-name">Prénom</label>
+              <input type="text" id="first-name" name="first-name" placeholder="Prénom" onClick={handleFocus} onBlur={handleBlur} />
+            </div>
           </div>
 
-          <input type="email" name="email" placeholder="Email" onClick={handleFocus} onBlur={handleBlur} />
+          <div>
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" placeholder="Email" onClick={handleFocus} onBlur={handleBlur} />
+          </div>
 
-          <input type="password" name="password" placeholder="Mot de passe" onClick={handleFocus} onBlur={handleBlur} />
+          <div>
+            <label htmlFor="password">Mot de passe</label>
+            <input type="password" id="password" name="password" placeholder="Mot de passe" onClick={handleFocus} onBlur={handleBlur} />
+          </div>
 
-          <input type="password" name="password-confirm" placeholder="Confirmer votre mot de passe" onClick={handleFocus} onBlur={handleBlur} />
+          <div>
+            <label htmlFor="password-confirm">Confirmer votre mot de passe</label>
+            <input type="password" id="password-confirm" name="password-confirm" placeholder="Confirmer votre mot de passe" onClick={handleFocus} onBlur={handleBlur} />
+          </div>
 
           <button type="submit">M'inscrire</button>
 
