c513458bf47c45d3476cbb4ad7101386b5bf32db
jerome <jerome.henry3@gmail.com>
2019-05-07 14:54:43 +0200

    minor code corrections

--- src/components/LeafletMap/Menu/index.js
+++ src/components/LeafletMap/Menu/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Menu, Dropdown, Icon, Button } from 'semantic-ui-react';
+import { Menu, Dropdown, Button } from 'semantic-ui-react';
 import Input from '../../Input';
 import PropTypes from 'prop-types';
 
@@ -7,33 +7,32 @@ import './menu.scss';
 
 const TopMenu = () => (
   <div id="menu">
-    {/* <Menu attached='top' borderless secondary> */}
-      <Dropdown item icon='bars' simple>
-        <Dropdown.Menu>
-          <Dropdown.Item>Déconnexion</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item>Mon compte</Dropdown.Item>
-          <Dropdown.Item>Autre chose ...?</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
+    <Dropdown item icon="bars" simple>
+      <Dropdown.Menu>
+        <Dropdown.Item>Déconnexion</Dropdown.Item>
+        <Dropdown.Divider />
+        <Dropdown.Item>Mon compte</Dropdown.Item>
+        <Dropdown.Item>Recrutement</Dropdown.Item>
+        <Dropdown.Item>A propos</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
 
-      <Menu.Menu>
-        <Button.Group>
-          <Button active>Carte</Button>
-          <Button className="no-border-left">Bâtiments</Button>
-        </Button.Group>
-      </Menu.Menu>
+    <Menu.Menu>
+      <Button.Group>
+        <Button active>Carte</Button>
+        <Button className="no-border-left">Bâtiments</Button>
+      </Button.Group>
+    </Menu.Menu>
 
-      <Menu.Menu position='right'>
-        <div className='ui right aligned category search item'>
-          <div className='ui transparent icon input'>
-            <Input className='prompt' type='text' placeholder='Recherche' />
-            <i className='search link icon' />
-          </div>
-          <div className='results' />
+    <Menu.Menu position="right">
+      <div className="ui right aligned category search item">
+        <div className="ui transparent icon input">
+          <Input className="prompt" type="text" placeholder="Recherche" />
+          <i className="search link icon" />
         </div>
-      </Menu.Menu>
-    {/* </Menu> */}
+        <div className="results" />
+      </div>
+    </Menu.Menu>
   </div>
 );
 
@@ -42,24 +41,3 @@ TopMenu.propTypes = {
 };
 
 export default TopMenu;
-
-    {/* <M>
-      <M.Item name='home' active={false} onClick={console.log} />
-      <M.Item
-        name='messages'
-        active={false}
-        onClick={console.log}
-      />
-      <M.Item
-        name='friends'
-        active={false}
-        onClick={console.log}
-      />
-      <M.Menu position='right'>
-        <M.Item
-          name='logout'
-          active={false}
-          onClick={console.log}
-        />
-      </M.Menu>
-    </M> */}
