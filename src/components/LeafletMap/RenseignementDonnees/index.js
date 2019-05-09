f422557e24599f81bea5dd7f681723928619e96a
Fabien Mackowiak <fmackodev@gmail.com>
2019-05-09 14:11:23 +0200

    Envoi de l'image au back

--- src/components/LeafletMap/RenseignementDonnees/index.js
+++ src/components/LeafletMap/RenseignementDonnees/index.js
@@ -38,6 +38,20 @@ const RenseignementDonnees = ({
     updateFormField('architectureInput', parseInt(e.target.value));
   };
 
+  const handleFileChange = (e) => {
+    const readFile = () => {
+      const reader = new FileReader();
+      reader.addEventListener('load', () => {
+        updateFormField('fileInput', reader.result);
+      });
+      reader.readAsDataURL(e.target.files[0]);
+    };
+
+    if (readFile) {
+      readFile(e.target.files[0]);
+    }
+  };
+
   const handleSubmitBuilding = (e) => {
     e.preventDefault();
     console.log('submitting building');
@@ -49,7 +63,7 @@ const RenseignementDonnees = ({
       <div className="renseignement-donnees_relative">
         <a href="#" className="renseignement-donnees_close" onClick={handleCloseDataForm}>Fermer</a>
         <label htmlFor="picture-building"><img src={AppareilPhoto} alt="Appareil" className="renseignement-donnees_appareil" /></label>
-        <input type="file" id="picture-building" className="inputfile" onChange={e => updateFormField('fileInput', e.target.files[0])} />
+        <input type="file" id="picture-building" className="inputfile" onChange={handleFileChange} accept="image/*" />
         <form action="">
           <div className="renseignement-donnees_inputs">
             <div className="renseignement-donnees_primary-infos">
@@ -88,7 +102,7 @@ const RenseignementDonnees = ({
                 disabled={false}
               />
             </div>
-            <div className="renseignement-donnees_secondary-infos">  
+            <div className="renseignement-donnees_secondary-infos">
               <div className="input-container">
                 <select
                   id="style"
@@ -129,7 +143,7 @@ const RenseignementDonnees = ({
                 />
                 <label htmlFor="date">Date</label>
               </div>
-          </div>
+            </div>
             <Input
               type="text"
               id="architect"
@@ -196,7 +210,7 @@ const RenseignementDonnees = ({
           </div>
         </form>
         <div className="footer-mobile">
-          <a href="#" className="renseignement-donnees_share">Partager <Icon name='share' /> </a>
+          <a href="#" className="renseignement-donnees_share">Partager <Icon name="share" /> </a>
           <a href="#" className="invisible-desktop btn-submit-mobile" onClick={handleSubmitBuilding}>Valider</a>
         </div>
       </div>
