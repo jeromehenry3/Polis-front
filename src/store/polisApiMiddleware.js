b497e0973b864556f72f1b41e27e7b1f63fe54d3
Fabien Mackowiak <fmackodev@gmail.com>
2019-05-14 10:38:06 +0200

    Correction emplacement markers + envoie donnée delivered au back

--- src/store/polisApiMiddleware.js
+++ src/store/polisApiMiddleware.js
@@ -95,7 +95,7 @@ const polisApiMiddleware = store => next => (action) => {
         urbanist: store.getState().urbanistInput,
         description: store.getState().descriptionInput,
         certified: false,
-        delivered: parseInt(store.getState().dateInput) < date.getFullYear(),
+        delivered: store.getState().dateInput === '' ? true : parseInt(store.getState().dateInput) < date.getFullYear(),
       }, {
         headers: {
           Authorization: `Bearer ${store.getState().token}`,
