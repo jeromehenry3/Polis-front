175236be328ba690ed5a9c7019ba49213fca35d4
jerome <jerome.henry3@gmail.com>
2019-06-16 14:06:52 +0200

    few changes

--- tests/Login.test.js
+++ tests/Login.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Redirect } from 'react-router-dom';
 import { should, expect } from 'chai';
 import { shallow, mount } from 'enzyme';
 import Login from '../src/components/Login';
@@ -8,12 +9,13 @@ should();
 
 describe('<Login />', () => {
   const ComponentWrapper = shallow(<Login />);
+  // const ComponentMounted = mount(<Login />);
   it('Should have an id "login"', () => {
     ComponentWrapper.props().should.have.property('id').which.equal('login');
   });
-  // it('Should contain a form', () => {
-  //   expect(ComponentWrapper.find(Form).find(<form />)).to.have.lengthOf(1);
-  // });
+  it('Should contain a form', () => {
+    expect(ComponentWrapper.render().find(<Form />)).to.have.lengthOf(1);
+  });
   // it('Should have a classname "clicked" when <Click /> receives a click event', () => {
   //   ComponentWrapper.props().should.have.property('className').which.is.empty;
 
