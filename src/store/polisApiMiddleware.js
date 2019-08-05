d798846e005777160d6362f661d00fd6dfc7f690
jerome <jerome.henry3@gmail.com>
2019-08-05 13:50:17 +0200

    changed url

--- src/store/polisApiMiddleware.js
+++ src/store/polisApiMiddleware.js
@@ -33,7 +33,7 @@ import {
   USER_VOTE,
 } from './reducer';
 
-const polisApi = 'https://api.polis-app.fr';
+const polisApi = 'https://api.polis-app.fr/api';
 // eslint-disable-next-line consistent-return
 const polisApiMiddleware = store => next => (action) => {
   switch (action.type) {
